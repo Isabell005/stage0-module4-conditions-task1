096810b042bb2056ee01f92da3279a8879064c88
Isabella Karibian <isabellakaribian@gmail.com>
2023-07-13 11:37:41 +0400

    stage0Module4conditionsTask1

--- src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task1;
 
 public class PositiveNumberDeterminer {
     public void isPositive(int numberToBeDetermined) {
+        numberToBeDetermined = 10/2;
+        if (numberToBeDetermined!=5){
+            System.out.println("false");
+        } else {
+            System.out.println("true");
+        }
     }
+
 }
